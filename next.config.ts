729e1137312ab6ce19dc1ba9a5c4641903b7000c
Oluwatimileyin-Favour <favourobagbuwa@yahoo.com>
2025-02-10 10:38:29 +0200

    test amplify

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   env: {
-    DATABASE_URL: process.env.DATABASE_URL 
+    DATABASE_URL: process.env.DATABASE_URL ?? ""
   },
 };
 
