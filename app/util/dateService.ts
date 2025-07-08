20e8ddefcf6bb35e3e7512b59ae23877707f8465
Oluwatimileyin-Favour <favourobagbuwa@yahoo.com>
2025-07-08 10:09:44 +0200

    changed potm table to july rankings

--- app/util/dateService.ts
+++ app/util/dateService.ts
@@ -1,4 +1,4 @@
 export function isDateInCurrentMonth(date: Date): boolean {
     const now = new Date();
-    return date.getFullYear() === now.getFullYear() && date.getMonth() === now.getMonth()-1;
+    return date.getFullYear() === now.getFullYear() && date.getMonth() === now.getMonth();
 }
